c5a2ae8c6bd23b40b9d76a4d8666ed789024512b
Vu Quang Duy <vuquangduy2017@gmail.com>
2023-06-23 10:57:18 +0700

    test file

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+//Dấu ngã đại diện cho src
+import myAlert from "~/myAlert"
+import React from "react"
+function Button(){
+    return <button onClick={myAlert}>Click me!</button>
+}
+export default Button
