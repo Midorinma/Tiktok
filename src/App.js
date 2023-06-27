e624a1752d7949c11c465ecbd89f579d0002abef
Vu Quang Duy <vuquangduy2017@gmail.com>
2023-06-27 11:59:26 +0700

    router/layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Button from './components/Button';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
 function App() {
-    return(
-        <div>
-            <Button/>
-            <h1>Chào bạn</h1>
-        </div>
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout===null? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-//Dấu ngã đại diện cho src
-import myAlert from "~/myAlert"
-import React from "react"
-function Button(){
-    return <button onClick={myAlert}>Click me!</button>
-}
-export default Button
